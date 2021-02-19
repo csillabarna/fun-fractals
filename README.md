342351aa99aa91b234e764bcf5a7ca8fb4b5a5e2
Csilla Barna <barnacsilla89@gmail.com>
2021-02-19 17:35:07 +0000

    Sierpiński triangle

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Fun Fractals
 
 Experimenting with recursion and graphics with the help of fractals.
+
+## Sierpiński triangle
+- [Wikipedia](https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle)
--- fractals.js
+++ fractals.js
@@ -17,8 +17,33 @@ function drawTriangle(a, b, c, style) {
     context.stroke()
 }
 
+function middle(a, b) {
+    const x = a.x + (b.x - a.x) / 2
+    const y = a.y - (a.y - b.y) / 2
+    return { x: x, y: y }
+}
+
+// Sierpiński triangle
+function drawFractalTriangle(a, b, c, n, style1, style2) {
+    if (n === 0) {
+        drawTriangle(a, b, c, style1)
+    }
+    else {
+        const abMid = middle(a, b)
+        const bcMid = middle(b, c)
+        const acMid = middle(c, a)
+
+        drawTriangle(a, b, c, style2)
+        drawFractalTriangle(a,     abMid, acMid, n - 1, style1, style2)
+        drawFractalTriangle(abMid, b,     bcMid, n - 1, style1, style2)
+        drawFractalTriangle(acMid, bcMid, c,     n - 1, style1, style2)
+    }
+}
+
 const a = { x: 0, y: canvas.height }
 const b = { x: canvas.width / 2, y: 0 }
 const c = { x: canvas.width, y: canvas.height }
-const style = { lineWidth: 1, lineColour: "black", fillColour: "orange" }
-drawTriangle(a, b, c, style)
+const n = 6
+const style1 = { lineWidth: 1, lineColour: "black", fillColour: "orange" }
+const style2 = { lineWidth: 1, lineColour: "black", fillColour: "yellow" }
+drawFractalTriangle(a, b, c, n, style1, style2)
